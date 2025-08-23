236e5cc00989c6a9358d99ea62720bbce77c036f
Hunter <hamptoncode@gmail.com>
2025-08-23 10:41:38 -0500

    Remove unused content, add links to footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://hn7r.github.io',
-    base: '/bible-tidbits',
+    base: '/bible-tidbits/',
 	integrations: [mdx(), sitemap()],
 });
